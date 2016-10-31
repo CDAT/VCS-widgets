07d21ecf683bf625c32bd2f309ca3c2019f764de
Sam Fries <fries2@llnl.gov>
2016-10-31 10:18:25 -0700

    Changed export to allow imports of GMEditForm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import GraphicsMethodEditForm from "./graphicsmethods"
 
-const widgets = {
-    GMEdit: GraphicsMethodEditForm,
-};
-
-export default widgets;
+export {
+    GraphicsMethodEditForm,
+}
