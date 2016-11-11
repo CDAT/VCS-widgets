6f1254eab428cb3f387f24846465eb0375b4e557
embrown <brown308@llnl.gov>
2016-11-11 07:50:08 -0800

    Fixing DatawcCoordinates

--- src/graphicsmethods/DatawcCoordinates.jsx
+++ src/graphicsmethods/DatawcCoordinates.jsx
@@ -26,10 +26,10 @@ var DatawcCoordinates = React.createClass({
     },
     getInitialState(){
         return {
-            x1: '',
-            x2: '',
-            y1: '',
-            y2: ''
+            x1: this.props.x1,
+            x2: this.props.x2,
+            y1: this.props.y1,
+            y2: this.props.y2
         }
     },
     componentWillReceiveProps(nextProps) {
@@ -68,55 +68,63 @@ var DatawcCoordinates = React.createClass({
         return (
             <div>
                 <div className="row">
+                    <div className='col-md-6'>
                     <h5> datawc_x1: </h5>
-                    <input type="text"
-                        name="datawc_x1"
-                        value={
-                            typeof this.state.x1 === "number" && this.state.x1 > 1e4
-                            ? this.state.x1.toExponential()
-                            : this.state.x1
-                        }
-                        onChange={(event)=>this.setState({x1: event.target.value})}
-                        onBlur={this.handleBlur}/>
-                    <Usage usage="Longitude value representing where the plot's x axis should start"/>
-                    <h5>datawc_x2: </h5>
-                    <input type="text"
-                        name="datawc_x2"
-                        value={
-                            typeof this.state.x2 === "number" && this.state.x2 > 1e4
-                            ? this.state.x2.toExponential()
-                            : this.state.x2
-                        }
-                        onChange={(event)=>this.setState({x2: event.target.value})}
-                        onBlur={this.handleBlur}/>
-                    <Usage id="x2-usage"
-                        usage="Longitude value representing where the plot's x axis should end"/>
+                        <input type="text"
+                            name="datawc_x1"
+                            value={
+                                typeof this.state.x1 === "number" && this.state.x1 > 1e4
+                                ? this.state.x1.toExponential()
+                                : this.state.x1
+                            }
+                            onChange={(event)=>this.setState({x1: event.target.value})}
+                            onBlur={this.handleBlur}/>
+                        <Usage usage="Longitude value representing where the plot's x axis should start"/>
+                    </div>
+                    <div className='col-md-6'>
+                        <h5>datawc_x2: </h5>
+                        <input type="text"
+                            name="datawc_x2"
+                            value={
+                                typeof this.state.x2 === "number" && this.state.x2 > 1e4
+                                ? this.state.x2.toExponential()
+                                : this.state.x2
+                            }
+                            onChange={(event)=>this.setState({x2: event.target.value})}
+                            onBlur={this.handleBlur}/>
+                        <Usage id="x2-usage"
+                            usage="Longitude value representing where the plot's x axis should end"/>
+                    </div>
                 </div>
                 <div className="row">
-                    <h5>datawc_y1: </h5>
-                    <input type="text"
-                        name="datawc_y1"
-                        value={
-                            typeof this.state.y1 === "number" && this.state.y1 > 1e4
-                            ? this.state.y1.toExponential()
-                            : this.state.y1
-                        }
-                        onChange={(event)=>this.setState({y1: event.target.value})}
-                        onBlur={this.handleBlur}/>
-                    <Usage id="y1-usage"
-                        usage="Latitude value representing where the plot's y axis should start"/>
-                    <h5>datawc_y2: </h5>
-                    <input type="text"
-                        name="datawc_y2"
-                        value={
-                            typeof this.state.y2 === "number" && this.state.y2 > 1e4
-                            ? this.state.y2.toExponential()
-                            : this.state.y2
-                        }
-                        onChange={(event)=>this.setState({y2: event.target.value})}
-                        onBlur={this.handleBlur}/>
-                    <Usage id="y2-usage"
-                        usage="Latitude value representing where the plot's y axis should end"/>
+                    <div className='col-md-6'>
+                        <h5>datawc_y1: </h5>
+                        <input type="text"
+                            name="datawc_y1"
+                            value={
+                                typeof this.state.y1 === "number" && this.state.y1 > 1e4
+                                ? this.state.y1.toExponential()
+                                : this.state.y1
+                            }
+                            onChange={(event)=>this.setState({y1: event.target.value})}
+                            onBlur={this.handleBlur}/>
+                        <Usage id="y1-usage"
+                            usage="Latitude value representing where the plot's y axis should start"/>
+                    </div>
+                    <div className='col-md-6'>
+                        <h5>datawc_y2: </h5>
+                        <input type="text"
+                            name="datawc_y2"
+                            value={
+                                typeof this.state.y2 === "number" && this.state.y2 > 1e4
+                                ? this.state.y2.toExponential()
+                                : this.state.y2
+                            }
+                            onChange={(event)=>this.setState({y2: event.target.value})}
+                            onBlur={this.handleBlur}/>
+                        <Usage id="y2-usage"
+                            usage="Latitude value representing where the plot's y axis should end"/>
+                    </div>
                 </div>
             </div>
         );
