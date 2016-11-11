828fa02767464f2273fc5c34643d01b5a5aadf7b
embrown <brown308@llnl.gov>
2016-11-11 08:36:16 -0800

    improving DatawcCoordinates layout

--- src/graphicsmethods/DatawcCoordinates.jsx
+++ src/graphicsmethods/DatawcCoordinates.jsx
@@ -80,60 +80,62 @@ var DatawcCoordinates = React.createClass({
         return (
             <div className='col-md-12'>
                 <h4>World Coordinates</h4>
-                <div className="row">
-                    <div className='col-md-6'>
-                    <h5> datawc_x1: </h5>
-                        <input type="text"
-                            name="datawc_x1"
-                            value={
-                                typeof this.state.x1 === "number" && this.state.x1 > 1e4
-                                ? this.state.x1.toExponential()
-                                : this.state.x1
-                            }
-                            onChange={(event)=>this.setState({x1: event.target.value})}
-                            onBlur={this.handleBlur}/>
-                        <Usage id="x1-usage" usage={x1_usage}/>
+                <div className='col-md-12'>
+                    <div className="row">
+                        <div className='col-md-6'>
+                        <h5> datawc_x1: </h5>
+                            <input type="text"
+                                name="datawc_x1"
+                                value={
+                                    typeof this.state.x1 === "number" && this.state.x1 > 1e4
+                                    ? this.state.x1.toExponential()
+                                    : this.state.x1
+                                }
+                                onChange={(event)=>this.setState({x1: event.target.value})}
+                                onBlur={this.handleBlur}/>
+                            <Usage id="x1-usage" usage={x1_usage}/>
+                        </div>
+                        <div className='col-md-6'>
+                            <h5>datawc_x2: </h5>
+                            <input type="text"
+                                name="datawc_x2"
+                                value={
+                                    typeof this.state.x2 === "number" && this.state.x2 > 1e4
+                                    ? this.state.x2.toExponential()
+                                    : this.state.x2
+                                }
+                                onChange={(event)=>this.setState({x2: event.target.value})}
+                                onBlur={this.handleBlur}/>
+                            <Usage id="x2-usage" usage={x2_usage}/>
+                        </div>
                     </div>
-                    <div className='col-md-6'>
-                        <h5>datawc_x2: </h5>
-                        <input type="text"
-                            name="datawc_x2"
-                            value={
-                                typeof this.state.x2 === "number" && this.state.x2 > 1e4
-                                ? this.state.x2.toExponential()
-                                : this.state.x2
-                            }
-                            onChange={(event)=>this.setState({x2: event.target.value})}
-                            onBlur={this.handleBlur}/>
-                        <Usage id="x2-usage" usage={x2_usage}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className='col-md-6'>
-                        <h5>datawc_y1: </h5>
-                        <input type="text"
-                            name="datawc_y1"
-                            value={
-                                typeof this.state.y1 === "number" && this.state.y1 > 1e4
-                                ? this.state.y1.toExponential()
-                                : this.state.y1
-                            }
-                            onChange={(event)=>this.setState({y1: event.target.value})}
-                            onBlur={this.handleBlur}/>
-                        <Usage id="y1-usage" usage={y1_usage}/>
-                    </div>
-                    <div className='col-md-6'>
-                        <h5>datawc_y2: </h5>
-                        <input type="text"
-                            name="datawc_y2"
-                            value={
-                                typeof this.state.y2 === "number" && this.state.y2 > 1e4
-                                ? this.state.y2.toExponential()
-                                : this.state.y2
-                            }
-                            onChange={(event)=>this.setState({y2: event.target.value})}
-                            onBlur={this.handleBlur}/>
-                        <Usage id="y2-usage" usage={y2_usage}/>
+                    <div className="row">
+                        <div className='col-md-6'>
+                            <h5>datawc_y1: </h5>
+                            <input type="text"
+                                name="datawc_y1"
+                                value={
+                                    typeof this.state.y1 === "number" && this.state.y1 > 1e4
+                                    ? this.state.y1.toExponential()
+                                    : this.state.y1
+                                }
+                                onChange={(event)=>this.setState({y1: event.target.value})}
+                                onBlur={this.handleBlur}/>
+                            <Usage id="y1-usage" usage={y1_usage}/>
+                        </div>
+                        <div className='col-md-6'>
+                            <h5>datawc_y2: </h5>
+                            <input type="text"
+                                name="datawc_y2"
+                                value={
+                                    typeof this.state.y2 === "number" && this.state.y2 > 1e4
+                                    ? this.state.y2.toExponential()
+                                    : this.state.y2
+                                }
+                                onChange={(event)=>this.setState({y2: event.target.value})}
+                                onBlur={this.handleBlur}/>
+                            <Usage id="y2-usage" usage={y2_usage}/>
+                        </div>
                     </div>
                 </div>
             </div>
